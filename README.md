99635e432b3905b46cbde49152d806d30e629c31
Anthony Chu <anthony@anthonychu.ca>
2014-06-21 23:24:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿Angular fill-height directive
+====
+
+Angular directive to change the height of a div to fill the available space in the window.
+
---
+++ src/fill-height.js
@@ -0,0 +1,54 @@
+﻿(function (fillHeightModule) {
+
+    fillHeightModule.directive('fillHeight', ['$window', '$document', function ($window, $document) {
+        return {
+            restrict: 'A',
+            scope: {
+                footerElementId: '@',
+                additionalPadding: '@'
+            },
+            link: function (scope, element, attrs) {
+                angular.element($window).on('resize', onWindowResize);
+                onWindowResize();
+
+                function onWindowResize() {
+                    var footerElement = angular.element($document[0].getElementById(scope.footerElementId));
+                    var footerElementHeight = footerElement[0].offsetHeight
+                                                + getTopMarginAndBorderHeight(footerElement)
+                                                + getBottomMarginAndBorderHeight(footerElement);
+
+                    var elementOffsetTop = element[0].offsetTop;
+                    var elementBottomMarginAndBorderHeight = getBottomMarginAndBorderHeight(element);
+
+                    var additionalPadding = scope.additionalPadding || 0;
+
+                    var elementHeight = $window.innerHeight
+                                        - elementOffsetTop
+                                        - elementBottomMarginAndBorderHeight
+                                        - footerElementHeight
+                                        - additionalPadding;
+
+                    console.log(elementHeight);
+                    element.css('height', elementHeight + 'px');
+                }
+
+                function getTopMarginAndBorderHeight(element) {
+                    var footerTopMarginHeight = getCssNumeric(element, 'margin-top');
+                    var footerTopBorderHeight = getCssNumeric(element, 'border-top-width');
+                    return footerTopMarginHeight + footerTopBorderHeight;
+                }
+
+                function getBottomMarginAndBorderHeight(element) {
+                    var footerBottomMarginHeight = getCssNumeric(element, 'margin-bottom');
+                    var footerBottomBorderHeight = getCssNumeric(element, 'border-bottom-width');
+                    return footerBottomMarginHeight + footerBottomBorderHeight;
+                }
+
+                function getCssNumeric(element, propertyName) {
+                    return parseInt(element.css(propertyName), 10) || 0;
+                }
+            }
+        };
+    }]);
+
+})(angular.module("fillHeight", []))
